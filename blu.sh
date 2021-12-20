4c38a9e1bd4eebcb6cc0da6ca702f64857714fa9
Oscar Vestlund <ove@lysator.liu.se>
2021-12-20 08:10:31 +0100

    Add menu generation

--- blu.sh
+++ blu.sh
@@ -5,20 +5,6 @@
 rm gen/*
 #old files removed
 
-#initialize and setup categories for menu entries
-#cats=$(grep -v "#" settings/categories.dat | grep : | awk '{print $1}')
-#cp materials/menu gen/menu.tmp
-#for Line in $cats
-#do
-#	itemname=$(grep "$Line" categories.dat | awk -F":" '{printf $3}')
-#	#sed "s#MENUITEM#$Line#g" materials/mbracket | echo >> gen/menu.tmp #gives empty output, fix this l8r
-#	sed "s#MENUITEM#$Line#" materials/mbracket | sed "s#ITEMNAME#$itemname#" > gen/item.tmp
-#	cat gen/item.tmp >> gen/menu.tmp
-#done
-#cat gen/menu.tmp materials/closemenu > menu.blu
-#echo "menu generated"
-#menu generated
-
 #starting to categorize html pages
 for file in txt/*.md
 do
@@ -27,6 +13,15 @@ do
 done
 echo "subcategories and tags generated"
 #everything is now categorised, html generation can start
+
+categories=$(find gen/*.cat | awk -F"/" '{printf $2}' | awk -F"." '{printf $1}')
+for Line in $categories
+do
+	itemname=$(cat txt/"$categories".md | grep "!title:" | awk -F":" '{print $1}')
+	sed "s#MENUITEM#$Line#" materials/mbracket | sed "s#ITEMNAME#$itemname#" > gen/menuitem.tmp
+	cat gen/menuitem.tmp >> gen/menu.tmp
+done
+echo "menu created"
 
 #filter out rare tags
 #for file in gen/*.tag
@@ -39,18 +34,6 @@ echo "subcategories and tags generated"
 #done
 #rare tags filtered out
 
-#turn all tags into a big page
-#cat menu.blu materials/bodyprefix > html/tags.html
-#for file in gen/*.tag
-#do
-#	filename=$(echo "${file##*/}" $file | awk -F"." '{printf $1}')
-#	echo "<p><b>$filename</b><br>" >> html/tags.html
-#	cat $file >> html/tags.html
-#	echo "</p>" >> html/tags.html
-#done
-#cat materials/bodysuffix materials/footer >> html/tags.html
-#tagsheets generated, all pages can be linked using tags
-
 #adding link sheets and catting html pages
 for file in txt/*.md
 do
@@ -58,10 +41,13 @@ do
 	#./aux.sh $file &
 	./blu.imp "$file" > gen/"$newfile".temp
 	#generates temporary pages and puts them in gen/
+
+	#post processing for markdown and moving files from gen/ to html/
+	filename=$( echo "$newfile" | awk -F"." '{printf $1}')
+	./blu.bmd gen/"$newfile".temp > html/$filename.html
+	#write html files to html directory
 done
 wait
-
-#rm gen/* #removes tmp files in gen/
 
 echo " "
 echo "finished!"
